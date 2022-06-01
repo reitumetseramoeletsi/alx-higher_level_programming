e440abfcf0a4a7fbf14761f21d62349f443b94df
Reitumetse <joycreamoeletsi@gmail.com>
2022-06-01 19:05:26 +0200

    check cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * check_cycle - Checks weather or no the list has a cycle in it
+ * @list: The given list for a check
+ *
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	int *buf;
+	unsigned int size;
+	unsigned int i;
+	unsigned int j;
+
+	size = print_listint(list);
+	buf = malloc(sizeof(int) * size);
+	
+	if (buf == NULL)
+	       exit(1);
+
+	for (i = 0; i < size; i++)
+	{
+		buf[i] = list->n;
+		list = list->next;
+		j = i;
+		while (j--)
+		{
+			if (list->n == buf[j])
+				return (1);
+		}
+	}
+
+	return(0);
+}
