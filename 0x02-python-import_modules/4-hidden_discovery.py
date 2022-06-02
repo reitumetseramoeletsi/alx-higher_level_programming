e77b56f3a606f17e79046f344c05dffa2848046e
Reitumetse <joycreamoeletsi@gmail.com>
2022-06-02 22:08:54 +0200

    print defined names

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import hidden_4
+
+
+def hide():
+    name = dir(hidden_4)
+
+    for i in name:
+        if i[:2] != "__":
+            print("{:s}".format(i))
+
+
+if __name__ == "__main__":
+    hide()
