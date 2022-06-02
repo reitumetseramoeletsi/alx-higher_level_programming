dd9b8bc4106d5c8a070b4abdcdde965a232f0a43
Reitumetse <joycreamoeletsi@gmail.com>
2022-06-02 20:27:32 +0200

    Argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import sys
+
+
+def args_1(argv):
+
+    n = len(argv) - 1
+
+    if n == 0:
+        print("{:d} argumennts.".format(n))
+        return
+    elif n == 1:
+        print("{:d} argument:".format(n))
+    else:
+        print("{:d} arguments:".format(n))
+
+    for i in range(1, n + 1):
+        print("{:d}: {:s}".format(i, argv[i]))
+
+
+if __name__ == "__main__":
+    args_1(sys.argv)
