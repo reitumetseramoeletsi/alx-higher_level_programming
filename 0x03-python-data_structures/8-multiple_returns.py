4cc0adfe4778e72fe71a5f3da49f464e4502f07b
Reitumetse <joycreamoeletsi@gmail.com>
2022-06-06 16:02:45 +0200

    maximum int

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    length = len(sentence)
+    if length != 0:
+        first = sentence[0]
+    else:
+        first is None
+    tuple_ab = (length, first)
+    return tuple_ab
