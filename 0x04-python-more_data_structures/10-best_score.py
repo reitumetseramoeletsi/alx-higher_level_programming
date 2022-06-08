956053085828274cf6318db6a1c191ffc1887202
Reitumetse <joycreamoeletsi@gmail.com>
2022-06-09 01:28:47 +0200

    More data structures

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None or a_dictionary == {}:
+    if a_dictionary is None or a_dictionary == []:
         return None
     mux = max(a_dictionary.values())
     for k, v in a_dictionary.items():
-        if value is mux:
+        if v is mux:
             return k
