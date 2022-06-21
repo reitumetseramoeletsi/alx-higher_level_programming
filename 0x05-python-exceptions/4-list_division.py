e2dadfee4c306d2753594f67435fe6f42a5f1762
Reitumetse <joycreamoeletsi@gmail.com>
2022-06-21 02:42:34 +0200

    exception handdling

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     new_list = [0]*list_length
+    if list_length <= 0:
+        print("out of range")
+        return new_list
+
     for i in range(0, list_length):
         div = 0
         try:
