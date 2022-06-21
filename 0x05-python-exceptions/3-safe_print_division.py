d215faadc64763f6ecf0961f8710bf0f1294d9d3
Reitumetse <joycreamoeletsi@gmail.com>
2022-06-21 02:58:41 +0200

    exception handdling

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    div = 0
+
     try:
-        print("Inside result: {:.1f}".format(a / b))
-        return a / b
+        div = a / b
     except ZeroDivisionError:
-        print("Inside result: None")
-        return None
+        div = None
+    finally:
+         print("Inside result: {}".format(div))
+    return div
