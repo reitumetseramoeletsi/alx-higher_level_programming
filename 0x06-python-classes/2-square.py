6d09933efff565285279d3fe2d9c7c532c95556e
Reitumetse <joycreamoeletsi@gmail.com>
2022-06-21 22:50:45 +0200

    Instanciate

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""2-Square with an instance"""
+
+
+class Square:
+    """Initialises Square with size as integer"""
+
+    def __init__(self, size=0):
+        """Size should be int"""
+
+        if type(size) != int:
+            raise TypeError("Size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
