06f9a4c7409999182203ac58ce2ac2bb95799cbe
Reitumetse <joycreamoeletsi@gmail.com>
2022-06-29 05:40:30 +0200

    update

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,4 @@
+#!/bin/python3
+def magic_string():
+    setattr(magic_string, "x", getattr(magic_string, "x", -1) + 1)
+    return "BestSchool" + ", BestSchool" * getattr(magic_string, "x", 0)
---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""module for LockedClass."""
+
+
+class LockedClass:
+    """A class demonstrating locked slots."""
+    __slots__ = 'first_name',
