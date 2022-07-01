ff14e63b6384cfd5337968179083aa5a9e74c12e
Reitumetse <joycreamoeletsi@gmail.com>
2022-07-01 20:27:14 +0200

    update

--- 0x07-python-test_driven_development/100-magic_string.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-def magic_string():
-    setattr(magic_string, "x", getattr(magic_string, "x", -1) + 1)
-    return "BestSchool" + ", BestSchool" * getattr(magic_string, "x", 0)
--- 0x07-python-test_driven_development/101-locked_class.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""dule for LockedClass."""
-
-
-class LockedClass:
-    '''A class demonstrating locked slots.'''
-    __slots__ = 'first_name',
