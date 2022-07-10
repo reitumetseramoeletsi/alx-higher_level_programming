9d42cba72b894410b425ee08a4a982b06ceb5024
reitumetse <joyceramoeletsi@gmail.com>
2022-07-10 15:57:50 +0200

    update

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This method determines if the class is the same
+"""
+
+
+def is_same_class(obj, a_class):
+    """The is_same_class method"""
+
+    return type(obj) is a_class
