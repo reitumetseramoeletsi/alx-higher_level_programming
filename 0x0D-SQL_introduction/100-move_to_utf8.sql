93a64db0446698efaa2eb36cbe5f3f9dd53ba02b
Reitumetse <joycreamoeletsi@gmail.com>
2022-08-10 23:49:06 +0200

    fix errors

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,4 @@
 -- Script that converts hbtn_0c_0 database to utf8
+USE hbtn_0c_0
 ALTER DATABASE hbtn_0c_0 CHARECTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table CONVERT TO CHARECTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
