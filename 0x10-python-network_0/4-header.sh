baaadf7d49cc649e0a738980c9e64d0ba94bd2b4
reitumetse <joyceramoeletsi@gmail.com>
2022-09-23 11:49:15 +0200

    update files

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script that sends a GET request to URL and displays body of response
+# This script sends a GET request to URL and displays body of response
 curl -sH "X-School-User-Id: 98" "$1"
